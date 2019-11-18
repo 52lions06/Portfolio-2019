4b3bcf2d7737514df460a87042513480f4456890
William McKelvey <williammckelvey@Williams-MacBook-Pro.local>
2019-11-18 06:47:09 -0500

    Updates

---
+++ precache-manifest.67d975f258c83b26f37c186e5f440ba5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca3592d781d89e3293ab36b4a41b774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13d898754f57b6a590a1",
+    "url": "/static/css/main.30cdb6e4.chunk.css"
+  },
+  {
+    "revision": "b8a1574c305a185be991",
+    "url": "/static/js/2.6b631bec.chunk.js"
+  },
+  {
+    "revision": "13d898754f57b6a590a1",
+    "url": "/static/js/main.e1cba9e0.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "d62d1444622fcc1a23ac9f9f57aae4b8",
+    "url": "/static/media/Belp.d62d1444.png"
+  },
+  {
+    "revision": "5c863f05ff73e46217b31ca6230c710e",
+    "url": "/static/media/Notehero.5c863f05.png"
+  },
+  {
+    "revision": "c7019cbbbd5a9f63fb6d95b077ba47ff",
+    "url": "/static/media/William_McKelvey_Developer*.c7019cbb.pdf"
+  },
+  {
+    "revision": "ac65677c7a2fe6af77b7f59b8041340f",
+    "url": "/static/media/happy.ac65677c.gif"
+  },
+  {
+    "revision": "54a19735390c8b0265d67e7efe925bcb",
+    "url": "/static/media/savage.54a19735.png"
+  },
+  {
+    "revision": "e1fb3f9d4bd1e8d4d6f852cc989a0b41",
+    "url": "/static/media/ultimategameplayer-pixel-webfont.e1fb3f9d.woff"
+  },
+  {
+    "revision": "ea8ae5e0f658c8d04f084143a0d5ff40",
+    "url": "/static/media/ultimategameplayer-pixel-webfont.ea8ae5e0.woff2"
+  },
+  {
+    "revision": "d433daa0112e81ff30f5d03abea55ffa",
+    "url": "/static/media/wm_black_badge.d433daa0.png"
+  }
+]);
